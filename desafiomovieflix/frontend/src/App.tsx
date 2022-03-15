5634ef5405bd60dbafd7affe92620e8d0acedc9a
auriceliof <auricelio.suporte@hotmail.com>
2022-03-15 19:40:40 -0300

    first commit

---
+++ desafiomovieflix/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import './assets/styles/custom.scss';
+
+function App() {
+  return (
+    <h1>Teste APP</h1>
+  );
+}
+
+export default App;
---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,42 @@
+import Navbar from "components/Navbar";
+import Admin from "pages/Admin";
+import Catalog from "pages/Catalog";
+import ProductDetails from "pages/ProductDetails";
+import Home from "pages/Home";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Auth from "pages/Admin/Auth";
+
+const Routes = () => (
+
+    <BrowserRouter>
+        <Navbar />
+        <Switch>
+            <Route path="/" exact>
+                <Home /> 
+            </Route>
+            
+            <Route path="/products" exact>
+                <Catalog />
+            </Route>
+
+            <Route path="/products/:productId">
+                <ProductDetails />
+            </Route>
+
+            <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+            <Route path="/admin/auth">
+                <Auth />
+            </Route>
+
+            <Redirect from="/admin" to="/admin/products" exact />
+            <Route path="/admin">
+                <Admin />
+            </Route>
+
+        </Switch>
+
+    </BrowserRouter>
+)
+
+export default Routes;
