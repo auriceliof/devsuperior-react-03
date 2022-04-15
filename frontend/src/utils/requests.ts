001821fa84611423145c382e3a5ab375edcccc86
auriceliof <auricelio.suporte@hotmail.com>
2022-04-15 20:56:32 -0300

    hasAnyRoles

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -112,3 +112,20 @@ export const isAuthenticated = () : boolean => {
   const TokenData = getTokenData();
   return (TokenData && TokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRoles = (roles : Role[]) : boolean => {
+  if ( roles.length === 0 ) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+  
+    if ( tokenData !== undefined ) {
+      for( var i = 0; i < roles.length; i++ ) {
+        if (tokenData.authorities.includes(roles[i])){
+          return true;
+        }
+      }
+    }
+    return false;
+}
