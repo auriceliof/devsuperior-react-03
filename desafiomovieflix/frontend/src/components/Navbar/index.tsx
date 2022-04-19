4f611f84be4daf8f5f8f49782de4928ab0a3d76b
auriceliof <auricelio.suporte@hotmail.com>
2022-04-18 21:32:05 -0300

    Styling Navbar

--- desafiomovieflix/frontend/src/components/Navbar/index.tsx
+++ desafiomovieflix/frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,9 @@
+import './styles.css';
+
 const Navbar = () => {
     return (
-        <nav className="bg-primary">
-            <div>
+        <nav className="bg-primary navbar-nav-container">
+            <div className='navbar-title'>
                 <a href="/">
                     <h2>MovieFlix</h2>
                 </a>
