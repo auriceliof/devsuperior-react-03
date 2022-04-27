b75a8b8718cf8a396f00d06c45978a6365986479
auriceliof <auricelio.suporte@hotmail.com>
2022-04-27 16:28:57 -0300

    Redirecting programmatically

--- desafiomovieflix/frontend/src/pages/Home/Login/index.tsx
+++ desafiomovieflix/frontend/src/pages/Home/Login/index.tsx
@@ -1,6 +1,7 @@
 import ButtonIcon from 'components/ButtonIcon';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 import { getAuthData, requestBackendLogin, saveAuthData } from 'utils/requests';
 import './styles.css';
 
@@ -15,6 +16,8 @@ const Login = () => {
 
   const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
+  const history = useHistory();
+
   const onSubmit = ( formData : FormData ) => {
     requestBackendLogin(formData)
     .then(response => {
@@ -23,6 +26,7 @@ const Login = () => {
       console.log('TOKEN GERADO: ' + token);
       setHasError(false);
       console.log('SUCESSO', response);
+      history.push('/movies');
     })
     .catch(error => {
       setHasError(true);
