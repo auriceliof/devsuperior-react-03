06193c6fea1b111e15bc1b7d4bb197f62375fbe4
auriceliof <auricelio.suporte@hotmail.com>
2022-04-27 16:11:51 -0300

    Authorization requests

---
+++ desafiomovieflix/frontend/src/types/review.ts
@@ -0,0 +1,10 @@
+import { User } from "./user";
+
+export type Movie = {
+    id: number;
+    text: string;
+    movieId: number;
+    user: User [];
+}
+
+
--- desafiomovieflix/frontend/src/utils/requests.ts
+++ desafiomovieflix/frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 
 export const BASE_URL = process.env.HEROKU_APP_BACKEND_URL ?? 'https://movieflix-devsuperior.herokuapp.com';
@@ -44,8 +44,18 @@ export const requestBackendLogin = ( loginData : LoginData ) => {
     });
 };
 
+export const requestBackend = ( config: AxiosRequestConfig ) => {
+    const headers = config.withCredentials
+        ? {
+            ...config.headers,
+            Authorization: 'Bearer ' + getAuthData().access_token,
+        }
+        : config.headers;
+    return axios({ ...config, baseURL: BASE_URL, headers });
+};
+
 export const saveAuthData = (obj: LoginResponse) => {
-    localStorage.setItem( 'authData', JSON.stringify(obj) );
+    localStorage.setItem( tokenKey, JSON.stringify(obj) );
 }
 
 export const getAuthData = () => {
